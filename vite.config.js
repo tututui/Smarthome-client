08cadbb2f0b3d07fee41ba4a2825fe2a41739380
SALFES <68468812+BunnySalf@users.noreply.github.com>
2025-01-02 22:15:31 +0700

    ad

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/src/',
   plugins: [react()],
+  base: '/', // เปลี่ยนเป็น '/' ถ้า deploy บน root domain
 })
